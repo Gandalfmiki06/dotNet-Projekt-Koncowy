57764a36f544c5fe4abe0927db1775607473e2b8
Stanisław Leszczyński <staszkekun@gmail.com>
2026-05-31 14:40:01 +0200

    init ASP.NET Core MVC project

---
+++ ClinicManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClinicManager.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
